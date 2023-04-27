a9f6e87e9c79fd53f6626a8ac7250c34f4d3d38d
Nathan <nathanarmand@hotmail.com>
2023-04-27 16:13:42 +0200

    Implementation of insert front of list

--- src/listes_dynamiques.c
+++ src/listes_dynamiques.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 
 Liste *initialiser(void) {
-	Liste *ptr = (Liste *) calloc(1, sizeof(Liste));
-	(*ptr).tete = NULL;
-	(*ptr).queue = NULL;
-	return ptr;
+    Liste *ptr = (Liste *) calloc(1, sizeof(Liste));
+    (*ptr).tete = NULL;
+    (*ptr).queue = NULL;
+    return ptr;
+}
+
+Status insererEnTete(Liste *liste, const Info *info) {
+
+    Element *ptr = (Element *) calloc(1, sizeof(Element));
+
+    if (!ptr) return MEMOIRE_INSUFFISANTE;
+
+    if (longueur(liste) == 1) liste->queue = ptr;
+
+    ptr->info = *info;
+    ptr->suivant = liste->tete;
+    ptr->precedent = NULL;
+    liste->tete = ptr;
+
+    return OK;
 }
