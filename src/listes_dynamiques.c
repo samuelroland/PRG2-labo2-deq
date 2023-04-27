18dee680f1aec3a2b82ce6a40278a9f06c8feaa5
G-LK <114987519+G-LK@users.noreply.github.com>
2023-04-27 16:25:21 +0200

    function "estVide" added

--- src/listes_dynamiques.c
+++ src/listes_dynamiques.c
@@ -3,52 +3,56 @@
 #include <stdlib.h>
 
 Liste *initialiser(void) {
-	Liste *ptr = (Liste *) calloc(1, sizeof(Liste));
-	if (!ptr) return NULL;
-	ptr->tete = NULL;
-	ptr->queue = NULL;
-	return ptr;
+    Liste *ptr = (Liste *) calloc(1, sizeof(Liste));
+    if (!ptr) return NULL;
+    ptr->tete = NULL;
+    ptr->queue = NULL;
+    return ptr;
+}
+
+bool estVide(const Liste *liste) {
+    return longueur(liste) == 0;
 }
 
 void afficher(const Liste *liste, Mode mode) {
 
-	if (estVide(liste)) {
-		printf("[]");
-		return;
-	}
+    if (estVide(liste)) {
+        printf("[]");
+        return;
+    }
 
-	if (mode == FORWARD) {
-		Element *courant = liste->tete;
-		printf("[");
-		for (size_t i = 0; i < longueur(liste); ++i) {
-			printf("%d", courant->info);
-			courant = courant->suivant;
-		}
-		printf("]");
+    if (mode == FORWARD) {
+        Element *courant = liste->tete;
+        printf("[");
+        for (size_t i = 0; i < longueur(liste); ++i) {
+            printf("%d", courant->info);
+            courant = courant->suivant;
+        }
+        printf("]");
 
-	} else {
-		Element *courant = liste->queue;
-		printf("[");
-		for (size_t i = longueur(liste); i > 0; --i) {
-			printf("%d", courant->info);
-			courant = courant->precedent;
-		}
-		printf("]");
-	}
+    } else {
+        Element *courant = liste->queue;
+        printf("[");
+        for (size_t i = longueur(liste); i > 0; --i) {
+            printf("%d", courant->info);
+            courant = courant->precedent;
+        }
+        printf("]");
+    }
 }
 
 Status insererEnTete(Liste *liste, const Info *info) {
 
-	Element *ptr = (Element *) calloc(1, sizeof(Element));
+    Element *ptr = (Element *) calloc(1, sizeof(Element));
 
-	if (!ptr) return MEMOIRE_INSUFFISANTE;
+    if (!ptr) return MEMOIRE_INSUFFISANTE;
 
-	if (longueur(liste) == 1) liste->queue = ptr;
+    if (longueur(liste) == 1) liste->queue = ptr;
 
-	ptr->info = *info;
-	ptr->suivant = liste->tete;
-	ptr->precedent = NULL;
-	liste->tete = ptr;
+    ptr->info = *info;
+    ptr->suivant = liste->tete;
+    ptr->precedent = NULL;
+    liste->tete = ptr;
 
-	return OK;
+    return OK;
 }
