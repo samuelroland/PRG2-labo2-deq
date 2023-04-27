22fb7effa79a37853fbb568c05ca682ef9359efa
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2023-04-27 16:18:31 +0200

    Fix initialiser() and merge

--- src/listes_dynamiques.c
+++ src/listes_dynamiques.c
@@ -1,56 +1,54 @@
 #include "../include/listes_dynamiques.h"
 #include <stdio.h>
-
-void afficher(const Liste *liste, Mode mode) {
-
-    if (estVide(liste)) {
-        printf("[]");
-        return;
-    }
-
-    if (mode == FORWARD) {
-        Element *courant = liste->tete;
-        printf("[");
-        for (size_t i = 0; i < longueur(liste); ++i) {
-            printf("%d", courant->info);
-            courant = courant->suivant;
-        }
-        printf("]");
-
-    } else {
-        Element *courant = liste->queue;
-        printf("[");
-        for (size_t i = longueur(liste); i > 0; --i) {
-            printf("%d", courant->info);
-            courant = courant->precedent;
-        }
-        printf("]");
-
-    }
-}
-
-
 #include <stdlib.h>
 
 Liste *initialiser(void) {
-    Liste *ptr = (Liste *) calloc(1, sizeof(Liste));
-    (*ptr).tete = NULL;
-    (*ptr).queue = NULL;
-    return ptr;
+	Liste *ptr = (Liste *) calloc(1, sizeof(Liste));
+	if (!ptr) return NULL;
+	ptr->tete = NULL;
+	ptr->queue = NULL;
+	return ptr;
+}
+
+void afficher(const Liste *liste, Mode mode) {
+
+	if (estVide(liste)) {
+		printf("[]");
+		return;
+	}
+
+	if (mode == FORWARD) {
+		Element *courant = liste->tete;
+		printf("[");
+		for (size_t i = 0; i < longueur(liste); ++i) {
+			printf("%d", courant->info);
+			courant = courant->suivant;
+		}
+		printf("]");
+
+	} else {
+		Element *courant = liste->queue;
+		printf("[");
+		for (size_t i = longueur(liste); i > 0; --i) {
+			printf("%d", courant->info);
+			courant = courant->precedent;
+		}
+		printf("]");
+	}
 }
 
 Status insererEnTete(Liste *liste, const Info *info) {
 
-    Element *ptr = (Element *) calloc(1, sizeof(Element));
+	Element *ptr = (Element *) calloc(1, sizeof(Element));
 
-    if (!ptr) return MEMOIRE_INSUFFISANTE;
+	if (!ptr) return MEMOIRE_INSUFFISANTE;
 
-    if (longueur(liste) == 1) liste->queue = ptr;
+	if (longueur(liste) == 1) liste->queue = ptr;
 
-    ptr->info = *info;
-    ptr->suivant = liste->tete;
-    ptr->precedent = NULL;
-    liste->tete = ptr;
+	ptr->info = *info;
+	ptr->suivant = liste->tete;
+	ptr->precedent = NULL;
+	liste->tete = ptr;
 
-    return OK;
+	return OK;
 }
